70ff8ef028453dbd6c15841ce970e7edbbd0eaab
andrewdorrington <dorringtonandrew30@gmail.com>
2026-03-26 14:46:04 +1100

    Refactor ClientFeedback component to use Next.js Script

--- src/components/ClientFeedback.tsx
+++ src/components/ClientFeedback.tsx
@@ -1,1 +1,15 @@
-<div id="featurable-1a83ce3d-e80e-4efb-a366-54d489b04474" data-featurable-async ></div><script src="https://featurable.com/assets/bundle.js" defer charset="UTF-8"></script>
+'use client';
+
+import Script from 'next/script';
+
+export default function Reviews() {
+  return (
+    <>
+      <div id="featurable-1a83ce3d-e80e-4efb-a366-54d489b04474" data-featurable-async></div>
+      <Script
+        src="https://featurable.com/assets/bundle.js"
+        strategy="lazyOnload"
+      />
+    </>
+  );
+}
