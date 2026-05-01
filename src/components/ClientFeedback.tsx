dd387ff5b0b022f4cfb6f7fa78b8d918147c06da
“andrewdorrington <dorringtonandrew30@gmail.com>
2026-05-01 12:25:07 +1000

    Refactor ClientFeedback component to dynamically load Featurable script on window load, improving performance and ensuring script is only added once.

--- src/components/ClientFeedback.tsx
+++ src/components/ClientFeedback.tsx
@@ -1,15 +1,37 @@
-'use client';
+import { useEffect } from 'react';
 
-import Script from 'next/script';
+const FEATURABLE_SCRIPT_SRC = 'https://featurable.com/assets/bundle.js';
 
-export default function Reviews() {
+export default function ClientFeedback() {
+  useEffect(() => {
+    const load = () => {
+      if (document.querySelector(`script[src="${FEATURABLE_SCRIPT_SRC}"]`)) return;
+      const script = document.createElement('script');
+      script.src = FEATURABLE_SCRIPT_SRC;
+      script.async = true;
+      document.body.appendChild(script);
+    };
+
+    if (document.readyState === 'complete') {
+      if (typeof requestIdleCallback === 'function') {
+        requestIdleCallback(load);
+      } else {
+        setTimeout(load, 0);
+      }
+    } else {
+      const onLoad = () => {
+        if (typeof requestIdleCallback === 'function') {
+          requestIdleCallback(load);
+        } else {
+          setTimeout(load, 0);
+        }
+        window.removeEventListener('load', onLoad);
+      };
+      window.addEventListener('load', onLoad);
+    }
+  }, []);
+
   return (
-    <>
-      <div id="featurable-1a83ce3d-e80e-4efb-a366-54d489b04474" data-featurable-async></div>
-      <Script
-        src="https://featurable.com/assets/bundle.js"
-        strategy="lazyOnload"
-      />
-    </>
+    <div id="featurable-1a83ce3d-e80e-4efb-a366-54d489b04474" data-featurable-async />
   );
 }
